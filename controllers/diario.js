ba885e3eb5b8766ddf6393807064add4af25f65a
VINICIUS RODRIGUES MARQUESIN <vinicius.marquesin>
2024-06-20 22:48:48 -0300

    diario

--- controllers/diario.js
+++ controllers/diario.js
@@ -26,21 +26,21 @@ module.exports = {
          });
       }
    },
-   async cadastrarUsuarios(request, response) {
+   async cadastrarDiario(request, response) {
       try {
          //parametros recebidos no corp da requisição
-         const { paciente_id, diario_relato, diario_data } =
+         const { diario_id, paciente_id, diario_relato, diario_data } =
             request.body;
          //instrução SQL
          const sql = `INSERT INTO diario
-            ( paciente_id, diario_relato, diario_data)
+            ( diario_id, paciente_id, diario_relato, diario_data)
             VALUES (?, ?, ?, ?)`;
          //definiçaõ dos dados a serem inseriodos em um array
-         const values = [paciente_id, diario_relato, diario_data];
+         const values = [diario_id, paciente_id, diario_relato, diario_data];
          //execução da instrução sql passando os parametros
          const execSql = await db.query(sql, values);
          //identificação do ID do resgistro inserido
-         const diario_id = execSql[0].insertId;
+         execSql[0].insertId;
 
          return response.status(200).json({
             sucesso: true,
@@ -56,31 +56,23 @@ module.exports = {
          });
       }
    },
-   async editarUsuarios(request, response) {
+   async editarDiario(request, response) {
       try {
          //parametro recebidos pelo corpo da requisição
-         const { paciente_id, diario_relato, diario_data } =
-            request.body;
+         const { paciente_id, diario_relato, diario_data } = request.body;
          //parametro recebido pela URl via params ex: /usuario/1
          const { diario_id } = request.params;
          //instruções SQL
-         const sql = `UPDATE usuarios SET usu_nome = ?, usu_nick = ?,
-            usu_email = ?, usu_senha = ?, usu_adm = ? WHERE usu_id = ?;`;
+         const sql = `UPDATE diario SET paciente_id = ?, diario_relato = ?,
+         diario_data = ? WHERE diario_id = ?;`;
          //preparo do array com dados que serão atualizados
-         const values = [
-            usu_nome,
-            usu_nick,
-            usu_email,
-            usu_senha,
-            usu_adm,
-            usu_id,
-         ];
+         const values = [paciente_id, diario_relato, diario_data, diario_id];
          //execução e obtenção de confirmação da atualização realizada
          const atualizaDados = await db.query(sql, values);
 
          return response.status(200).json({
             sucesso: true,
-            mensagem: `Usuário ${usu_id} atualizado com sucesso!`,
+            mensagem: `Diário ${diario_id} atualizado com sucesso!`,
             dados: atualizaDados[0].affectedRows,
             //mensSql: atualizaDAdos
          });
@@ -92,20 +84,20 @@ module.exports = {
          });
       }
    },
-   async apagarUsuarios(request, response) {
+   async apagarDiario(request, response) {
       try {
          //parametro passado via URL na chamada da api pelo front-end
-         const { usu_id } = request.params;
+         const { diario_id } = request.params;
          //comando da exclusão
-         const sql = `DELETE FROM usuarios WHERE usu_id = ?`;
+         const sql = `DELETE FROM diario WHERE diario_id = ?`;
          //array com parametros da exclusão
-         const values = [usu_id];
+         const values = [diario_id];
          //executa instrução no banco de dados
          const excluir = await db.query(sql, values);
 
          return response.status(200).json({
             sucesso: true,
-            mensagem: `Usuário ${usu_id} excluído com sucesso`,
+            mensagem: `Diário ${diario_id} excluído com sucesso`,
             dados: excluir[0].affectedRows,
          });
       } catch (error) {
@@ -139,37 +131,4 @@ module.exports = {
    //       });
    //    }
    // },
-   async login(request, response) {
-      try {
-         const { usu_email, usu_senha } = request.body;
-
-         const sql = `SELECT usu_id, usu_nome, usu_adm FROM usuarios 
-                WHERE usu_email = ? AND usu_senha = ?;`;
-
-         const values = [usu_email, usu_senha];
-
-         const usuarios = await db.query(sql, values);
-         const nItens = usuarios[0].length;
-
-         if (nItens < 1) {
-            return response.status(403).json({
-               sucesso: false,
-               mensagem: "Login e/ou senha inválido.",
-               dados: null,
-            });
-         }
-
-         return response.status(200).json({
-            sucesso: true,
-            mensagem: "Login efetuado com sucesso",
-            dados: usuarios[0],
-         });
-      } catch (error) {
-         return response.status(500).json({
-            sucesso: false,
-            mensagem: "Erro na requisição.",
-            dados: error.message,
-         });
-      }
-   },
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,36 +1,33 @@
-const express = require ('express');
+const express = require("express");
 const router = express.Router();
 
 //referencia a controllers que serão utilizados nas rotas
-const UsuariosController = require('../controllers/usuarios');
-const PacienteController = require('../controllers/pacientes')
+const UsuariosController = require("../controllers/usuarios");
+const PacienteController = require("../controllers/pacientes");
+const DiarioController = require("../controllers/diario");
 
 //  paciente
 
-router.get('/usuarios', UsuariosController.listarUsuarios);
-router.post('/usuarios', UsuariosController.cadastrarUsuarios);
-router.patch('/usuarios/:usu_id', UsuariosController.editarUsuarios);
-router.delete('/usuarios/:usu_id', UsuariosController.apagarUsuarios);
+router.get("/usuarios", UsuariosController.listarUsuarios);
+router.post("/usuarios", UsuariosController.cadastrarUsuarios);
+router.patch("/usuarios/:usu_id", UsuariosController.editarUsuarios);
+router.delete("/usuarios/:usu_id", UsuariosController.apagarUsuarios);
 // router.delete('/usuarios/del/:usu_id', UsuariosController.ocultarUsuario);
-router.post('/usuarios/login', UsuariosController.login);
+router.post("/usuarios/login", UsuariosController.login);
 
 // psicologo
 
-router.get('/paciente', PacienteController.listaPacientes);
-router.post('/paciente', PacienteController.cadastrarPacientes);
-router.patch('/paciente/:paciente_id', PacienteController.editarPacientes);
-router.delete('/paciente/:paciente_id', PacienteController.apagarPacientes);
+router.get("/paciente", PacienteController.listaPacientes);
+router.post("/paciente", PacienteController.cadastrarPacientes);
+router.patch("/paciente/:paciente_id", PacienteController.editarPacientes);
+router.delete("/paciente/:paciente_id", PacienteController.apagarPacientes);
 // router.post('/usuarios/login', UsuariosController.login);
 
 // diario
-router.get('/paciente', PacienteController.listaPacientes);
-router.post('/paciente', PacienteController.cadastrarPacientes);
-router.patch('/paciente/:paciente_id', PacienteController.editarPacientes);
-router.delete('/paciente/:paciente_id', PacienteController.apagarPacientes);
-// const Diario = require('../controllers/diario')
+router.get("/diario", DiarioController.listarDiario);
+router.post("/diario", DiarioController.cadastrarDiario);
+router.patch("/diario/:diario_id", DiarioController.editarDiario);
+router.delete("/diario/:diario_id", DiarioController.apagarDiario);
 
-router.get('')
-module.exports = router; 
-
-
-
+router.get("");
+module.exports = router;
