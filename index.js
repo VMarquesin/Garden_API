56aca7b1f73f58a2100b3bea04938b059cb99fae
VMarquesin <157024346+VMarquesin@users.noreply.github.com>
2024-09-16 22:50:52 -0300

    Commit

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.listen(porta, () => {
    console.log(`Servidor iniciado na porta ${porta}`);
 });
 
-app.get("/", (request, response) => {
-   response.send("Hello World");
-});
+// app.get("/", (request, response) => {
+//    response.send("Hello World");
+// });
